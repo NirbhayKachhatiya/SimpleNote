3f1b8a566a8501cd8573f98d9488203572b47b75
NirbhayKachhatiya <nirbhaykck@gmail.com>
2026-03-24 14:54:18 +0530

    Deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,33 @@ import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 //middlewares
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 app.use(express.json()); //to parse json bodies
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"))
+    })
+}
 
 connectDb().then(() => {
     app.listen(5000, () => {
-        console.log("Server is running on port 5000");
+        console.log("Server is running on port", PORT);
     });
 });
